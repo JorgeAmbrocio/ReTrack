e31f0de045af839a26823e118da595df961c3d3a
Jorge Ambrocio <jorge.ambrocio.ventura@gmail.com>
2022-11-16 20:50:54 -0600

    [ADD] maquetado de servicios

---
+++ backend/datamanager/source/app.py
@@ -0,0 +1,18 @@
+import json
+import sys
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+# FUNCTION TO TEST IF THE API IS WORKING
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify({'message': 'API datamanager is working!'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=7766)
